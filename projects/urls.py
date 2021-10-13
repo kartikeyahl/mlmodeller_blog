db1cd91f14837d0e8d9359b9452239e5adc501a1
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2021-10-13 08:15:00 +0530

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.projects, name="projects"),
-    path('project/<str:pk>/', views.project, name="project"),
+    path('blog/<str:pk>/', views.project, name="project"),
 
-    path('create-project/', views.createProject, name="create-project"),
+    path('create-blog/', views.createProject, name="create-project"),
 
-    path('update-project/<str:pk>/', views.updateProject, name="update-project"),
+    path('update-blog/<str:pk>/', views.updateProject, name="update-project"),
 
-    path('delete-project/<str:pk>/', views.deleteProject, name="delete-project"),
+    path('delete-blog/<str:pk>/', views.deleteProject, name="delete-project"),
 ]
