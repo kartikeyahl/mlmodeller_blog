a50e290081b78fb2ca6fafad5a171660e04d78e8
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2021-11-03 08:27:27 +0530

    Update views.py

--- regression/views.py
+++ regression/views.py
@@ -12,6 +12,9 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+
 
 def DecisionTreeRegression(request):
     context = {}
@@ -42,6 +45,13 @@ def DecisionTreeRegression(request):
             for res in result:
                 f_data.append(res)
             context['result'] = f_data
+            y_pred = classifier.predict(X_test)  
+            mse=mean_squared_error(y_test,y_pred)
+            rmse=np.sqrt(mean_squared_error(y_test,y_pred))
+            mae=mean_absolute_error(y_test,y_pred)
+            context['mse']= mse
+            context['rmse']= rmse
+            context['mae']= mae
         except Exception as e:
             return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request, template_name, context)
@@ -78,6 +88,13 @@ def MultipleLinearRegression(request):
             for res in result:
                 f_data.append(res)
             context['result'] = f_data
+            y_pred = classifier.predict(X_test)  
+            mse=mean_squared_error(y_test,y_pred)
+            rmse=np.sqrt(mean_squared_error(y_test,y_pred))
+            mae=mean_absolute_error(y_test,y_pred)
+            context['mse']= mse
+            context['rmse']= rmse
+            context['mae']= mae
         except Exception as e:
             return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request, template_name, context)
@@ -116,6 +133,13 @@ def PolynomialRegression(request):
             for res in result:
                 f_data.append(res)
             context['result'] = f_data
+            y_pred = classifier.predict(X_test)  
+            mse=mean_squared_error(y_test,y_pred)
+            rmse=np.sqrt(mean_squared_error(y_test,y_pred))
+            mae=mean_absolute_error(y_test,y_pred)
+            context['mse']= mse
+            context['rmse']= rmse
+            context['mae']= mae
         except Exception as e:
             return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request, template_name, context)
@@ -150,6 +174,13 @@ def RandomForestRegression(request):
             for res in result:
                 f_data.append(res)
             context['result'] = f_data
+            y_pred = classifier.predict(X_test)  
+            mse=mean_squared_error(y_test,y_pred)
+            rmse=np.sqrt(mean_squared_error(y_test,y_pred))
+            mae=mean_absolute_error(y_test,y_pred)
+            context['mse']= mse
+            context['rmse']= rmse
+            context['mae']= mae
         except Exception as e:
             return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request, template_name, context)
@@ -186,6 +217,13 @@ def SimpleLinearRegression(request):
             for res in result:
                 f_data.append(res)
             context['result'] = f_data
+            y_pred = classifier.predict(X_test)  
+            mse=mean_squared_error(y_test,y_pred)
+            rmse=np.sqrt(mean_squared_error(y_test,y_pred))
+            mae=mean_absolute_error(y_test,y_pred)
+            context['mse']= mse
+            context['rmse']= rmse
+            context['mae']= mae
         except Exception as e:
             return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request, template_name, context)
@@ -225,6 +263,13 @@ def SupportVectorRegression(request):
             for res in result:
                 f_data.append(res)
             context['result'] = f_data
+            y_pred = classifier.predict(X_test)  
+            mse=mean_squared_error(y_test,y_pred)
+            rmse=np.sqrt(mean_squared_error(y_test,y_pred))
+            mae=mean_absolute_error(y_test,y_pred)
+            context['mse']= mse
+            context['rmse']= rmse
+            context['mae']= mae
         except Exception as e:
             return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request, template_name, context)
