4cb9da6b3dc6198a8e291e89d6eedc654664c0f4
kartikeyahl <43701324+kartikeyahl@users.noreply.github.com>
2021-11-03 12:33:46 +0530

    Update views.py

--- regression/views.py
+++ regression/views.py
@@ -88,7 +88,11 @@ def MultipleLinearRegression(request):
             for res in result:
                 f_data.append(res)
             context['result'] = f_data
-            
+            y_pred = classifier.predict(X_test)  
+            mse=mean_squared_error(y_test,y_pred)
+            rmse=np.sqrt(mean_squared_error(y_test,y_pred))
+            mae= mean_absolute_error(y_test,y_pred)
+            context['mse']= mse
             
         except Exception as e:
             return HttpResponse("Error Occured , Reason : " + str(e))
