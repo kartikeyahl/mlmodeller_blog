e43b923c376802460e11c598cf4c7e1f5c40a02e
Kartikey Ahlawat <43701324+kartikeyahl@users.noreply.github.com>
2022-11-08 11:37:31 +0530

    Update views.py

--- regression/views.py
+++ regression/views.py
@@ -24,57 +24,36 @@ def DecisionTreeRegression(request):
     context['title'] = 'Decission Tree Regression'
     template_name = "Regression/Sub-Categories/Decision Tree Regression.html"
     if request.method == 'POST':
-        csv_file = request.FILES.get['data_file']
-        if not csv_file.name.endswith('.xlsx'):
+        csv_file = request.FILES['data_file']
+        input_csv_file = request.FILES['input_file']
+        if not csv_file.name.endswith('.csv'):
             messages.error(request, 'data file is not a valid CSV file')
             return redirect('decision-tree-regression')
-        if not input_csv_file.name.endswith('.xlsx'):
+        if not input_csv_file.name.endswith('.csv'):
             messages.error(request, 'input file is not a valid CSV file')
             return redirect('decision-tree-regression')
         try:
-            dataset = pd.read_excel(csv_file)
-            dataset.rename(columns={'IN/OUT': 'IN_OUT'}, inplace=True)
-            dataset=dataset[dataset.IN_OUT!=1]
-            j,k=0,0
-            for i in dataset.iloc[:,3]:
-              dataset.Date[j]=datetime.strptime(str(i),'%Y%m%d').date()
-              j+=1
-
-            dataset['Date'] = pd.to_datetime(dataset['Date'], errors='coerce')
-            l=[]
-            for i in range(len(dataset)-1):
-              j=i+1
-              l.append([dataset.iloc[i]['E.Code'], datetime.combine(dataset.Date[0], datetime.strptime(str(dataset.iloc[j]['Time']),'%H%M%S').time()) - datetime.combine(dataset.Date[0], datetime.strptime(str(dataset.iloc[i]['Time']),'%H%M%S').time())])  
-              i=i+2
-            for i in range(len(l)):
-                l[i].insert(1,dataset.iloc[i]['Date'].date())
-            for i in range(len(l)):
-              l[i][2] = l[i][2].__str__().replace(":",".")
-              s=l[i][2]
-              l[i][2]=s[0:-3]
-            l2=[]
-            for i in range(len(l)):
-              if 'd' not in str(l[i][2]):
-                l2.append(l[i])
-            for i in range(len(l2)):
-                l2[i][2]=float("{:.2f}".format(float(l2[i][2])))
-            l3=[]
-            for i in range(len(l2)):
-                frac, whole = math.modf(l2[i][2])
-                mins=int(whole*60+frac*100)
-                if mins>570:
-                  mins=mins-570
-                  hours=mins//60
-                  minutes=mins%60
-                  if minutes<10:
-                    ot_time = float("{}.0{}".format(hours, minutes))
-                  else:
-                    ot_time = float("{}.{}".format(hours, minutes))
-                  l2[i].append(ot_time)
-                  l3.append(l2[i])
-            df = pd.DataFrame(l3, columns=['E.Code','Date','Total Time','OT'])
-            df=df.drop(['Total Time'], axis=1)
-            context['df'] = df
+            dataset = pd.read_csv(csv_file)
+            lst = pd.read_csv(input_csv_file)
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+            imputer.fit(X[:, :])
+            X[:, :] = imputer.transform(X[:, :])
+            regressor = DecisionTreeRegressor(random_state=0)
+            regressor.fit(X, y)
+            result = regressor.predict(lst)
+            f_data = []
+            for res in result:
+                f_data.append(res)
+            context['result'] = f_data
+            '''y_pred = classifier.predict(X_test)  
+            mse=mean_squared_error(y_test,y_pred)
+            rmse=np.sqrt(mean_squared_error(y_test,y_pred))
+            mae= mean_absolute_error(y_test,y_pred)
+            context['mse']= mse
+            context['rmse']= rmse
+            context['mae']= mae'''
         except Exception as e:
             return HttpResponse("Error Occured , Reason : " + str(e))
     return render(request, template_name, context)
